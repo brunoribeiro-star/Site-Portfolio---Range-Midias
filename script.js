db330b00f2c570a4dd6757cfab4748a179194dcd
brunoribeiro-star <engs-brunoribeiro@camporeal.edu.br>
2024-04-13 00:01:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    netImage();
+},5000)
+
+function netImage() {
+    count++
+    if(count>3) {
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true; 
+
+}
