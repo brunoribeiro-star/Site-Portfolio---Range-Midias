e0ad34f2386f36400bdaccfc523fcdd266c03b45
brunoribeiro-star <engs-brunoribeiro@camporeal.edu.br>
2024-04-15 08:41:03 -0300

    'att'

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ function netImage() {
     document.getElementById("radio"+count).checked = true; 
 
 }
+
+const secondLine = document.getElementById("second-line");
+
+const phrases = [
+    "potencializar as suas vendas.",
+    "potencializar o seu alcance.",
+    "potencializar a sua visibilidade."
+];
+
+let currentIndex = 0;
+
+function glitchEffect(text) {
+    const words = text.split(" ");
+    const glitchedWords = words.map(word => `<span class="glitch">${word}</span>`).join(" ");
+    secondLine.innerHTML = glitchedWords;
+    secondLine.classList.add("glitch-animation");
+    setTimeout(() => {
+        secondLine.classList.remove("glitch-animation");
+    }, 500);
+}
+
+function changeText() {
+    const phrase = phrases[currentIndex];
+    glitchEffect(phrase);
+    currentIndex = (currentIndex + 1) % phrases.length;
+}
+
+
+changeText();
+
+
+setInterval(changeText, 3000);
+
+
